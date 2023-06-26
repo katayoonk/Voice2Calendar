f75a5dba4c893365d33a08054dd8bcdd1c2cde20
Katy <kaviany.katayoon@gmail.com>
2023-06-26 16:25:45 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Voice2Calendar
+
+Voice2Calendar is a Python-based solution designed to address a specific personal need. This program allows users to transform voice messages into a comprehensive to-do list and then have a calendar file sent to them via email. Here are some features:
+
+- **Voice Message Conversion**: Given the audio message from iMessage, the script converts the audio file from .caf to mp3 format using ffmpeg library.
+
+- **Speech to Text**: Given the audio file, Whisper model performs speech-to-text to generate a transcript of the given audio file.
+
+- **Making a to-do list**: Given the transcript, the text-davinci-003 model generates a to-do list out of it.
+
+- **Calendar Integration**: Given the to-do list, the script converted it into a calendar file in the standard .ics format using ics library.
+
+- **Emailing the Calendar File**: Given the ics file, the script sends an email including .ics file as attachment using EmailMessage library.
+
+## How It Works
+
+1. Clone the repository: `git clone https://github.com/katayoonk/Voice2Calendar`
+2. Copy the voice message from iMessage to the same folder. 
+3. Install the required dependencies: `pip install -r requirements.txt`
+4. Run the Python script: `python voice2calendar.py`
+6. The script converts the audio file format to ensure compatibility and ease of processing.
+7. The voice message is transcribed into text using Whisper model.
+8. The OpenAI api changes uses the transcript to generates a structured to-do list.
+9. A calendar file in .ics format is automatically created based on the to-do list.
+10. The calendar file is attached to an email and sent to the specified email address for seamless integration into popular calendar applications.
+11. Open the email, download the calendar file, and import it into your preferred calendar application.
+
+## Contributing
+
+Contributions to Voice2Calendar are welcome! If you have any ideas, suggestions, or bug reports, please feel free to open an issue or submit a pull request. Let's collaborate to enhance the functionality and usability of this project.
---
+++ makeToDoList.py
@@ -0,0 +1,116 @@
+import os
+import pydub
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+import whisper
+import subprocess
+from langchain.prompts import PromptTemplate
+from langchain.llms import OpenAI
+from langchain.chains import LLMChain
+from pyicloud import PyiCloudService
+from datetime import datetime
+import smtplib
+import ssl
+from email.message import EmailMessage
+from ics import Calendar, Event
+
+# Step 1 - Call Whisper to transcribe audio
+print("Calling Whisper to transcribe audio...\n")
+
+pydub.AudioSegment.converter = r"C:/Users/kavia/anaconda3/Library/bin/ffmpeg.exe"
+
+# convert .caf to .mp#
+# ffmpeg -i voice.caf voice.mp3
+def convertCAF2MP3(input, output):
+    command = []
+    command.append("ffmpeg -i ")
+    command.append(input)
+    # command.append(" -ac 2 -f wav -map_metadata 0 - | lame -V 2 -")
+    command.append(output)
+    command.append(".mp3 >/dev/null 2>&1")
+    return command
+subprocess.check_output(['ffmpeg','-i', 'voice.caf', 'voice.mp3'], shell=True)
+print('.caf file converted to .mp3')
+
+# Input the voice
+audio_file = "./voice.mp3"
+
+# Chunk up the audio file 
+sound_file = AudioSegment.from_mp3(audio_file)
+audio_chunks = split_on_silence(sound_file, min_silence_len=1000, silence_thresh=-40 )
+count = len(audio_chunks)
+print("Audio split into " + str(count) + " audio chunks")
+
+#Call Whisper to transcribe audio
+model = whisper.load_model("base")
+transcript = ""
+for i, chunk in enumerate(audio_chunks):
+    # If you have a long audio file, you can enable this to only run for a subset of chunks
+    if i < 10 or i > count - 10:
+        out_file = "chunk{0}.wav".format(i)
+        print("Exporting", out_file)
+        chunk.export(out_file, format="wav")
+        result = model.transcribe(out_file)
+        transcriptChunk = result["text"]
+        print(transcriptChunk)
+        
+        # Append transcript in memory if you have sufficient memory
+        transcript += " " + transcriptChunk
+        
+# make a to-do-list using gpt3
+os.environ["OPENAI_API_KEY"] = "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"
+
+# give gpt a prompt
+prompt = PromptTemplate(
+    input_variables=["transcript"],
+    template="Make a to-do list from {transcript} in a way that gmail recognizes it as a today's event and add the whole list to my calendar",
+)
+
+# print(f'prompt message: {prompt.format(message=transcript)}')
+
+# generating the answer
+llm = OpenAI(
+          model_name="text-davinci-003", # default model
+          temperature=0.9) #temperature dictates how whacky the output should be
+llmchain = LLMChain(llm=llm, prompt=prompt)
+todolist = llmchain.run(transcript)
+
+#make ics file from the to-do list
+c = Calendar()
+e = Event()
+e.name = f"{datetime.now().day}'s to-do list"
+e.begin = datetime.now()
+e.description = todolist
+c.events.add(e)
+# c.events
+with open('my.ics', 'w') as my_file:
+    my_file.writelines(c.serialize_iter())
+    
+#email the to-do list to myself
+email_sender = 'kaviany.katayoon@gmail.com'
+email_password = 'itzoqfuwhghauwpj'
+email_receiver = 'kaviany.katayoon@gmail.com'
+subject = "Today's To-do List"
+body = todolist
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['Subject'] = subject
+em.set_content(body)
+
+#adding attachment
+# Open the .ics file in binary mode and read its content
+with open('./my.ics', 'rb') as f:
+    file_data = f.read()
+
+em.add_attachment(file_data, maintype = 'ics', subtype = 'ics', filename = 'my.ics')
+
+#add SSL
+context = ssl.create_default_context()
+
+#Log in and send the email
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
+    
+print('\n Email sent successfully!')
